c9445114873a37baad1b63756bf860f7bd514d22
gokuthug1 <102106500+gokuthug1@users.noreply.github.com>
2025-08-11 20:41:29 -0500

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -477,7 +477,7 @@ const ChatApp = {
 
                 const copyCodeBtn = document.createElement('button');
                 copyCodeBtn.className = 'copy-code-button';
-                copyCodeBtn.innerHTML = `${COPY}<span>Copy Code</span>`;
+                copyCodeBtn.innerHTML = `${COPY}<span></span>`;
                 copyCodeBtn.addEventListener('click', e => {
                     e.stopPropagation();
                     navigator.clipboard.writeText(pre.textContent).then(() => {
